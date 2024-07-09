b668299d244a73d6163595437cb36fd5e28c779d
Jacob Thomas Redmond <127049997+Jrbiltmore@users.noreply.github.com>
2024-07-08 20:43:45 -0700

    Update truffle-config.js

---
+++ config/truffle-config.js
@@ -0,0 +1,60 @@
+// truffle-config.js
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+      gas: 6721975,          // Gas limit
+      gasPrice: 20000000000  // 20 Gwei
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 3,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`),
+      network_id: 1,       // Mainnet's id
+      gas: 5500000,        // Mainnet gas limit
+      gasPrice: 100000000000, // 100 Gwei
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false    // Skip dry run before migrations? (default: false for public nets )
+    }
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 200
+        },
+        evmVersion: "istanbul"
+      }
+    }
+  },
+
+  // Truffle DB is disabled by default; to enable it, change enabled: false to enabled: true
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // to keep using the artifacts you previously obtained, you need to manually set the field to enabled: false
+  // in those artifacts.
+  db: {
+    enabled: false
+  }
+};
